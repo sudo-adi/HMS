e0cf55df76da5be654a326913d185341d5b51a44
Aditya <sudo.aditya@gmail.com>
2025-02-11 21:27:28 +0530

    Initial Commit

---
+++ HMS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMS
+//
+//  Created by Aditya Prasad on 11/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HMS/HMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMSApp.swift
+//  HMS
+//
+//  Created by Aditya Prasad on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct HMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
