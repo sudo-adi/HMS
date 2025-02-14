b6b044e2612d9b822a917a67c200d89bb0b2e49f
Rishabh Raj <singhhappy89711@gmail.com>
2025-02-14 09:46:49 +0530

    Doctor Added

---
+++ HMS/Views/Auth/DoctorTodayScreenApp.swift
@@ -0,0 +1,15 @@
+//
+//  DoctorTodayScreenApp.swift
+//  HMS
+//
+//  Created by Purushotam Kumar on 14/02/25.
+//
+
+import SwiftUI
+struct DoctorTodayScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DoctorSchedule()
+        }
+    }
+}
